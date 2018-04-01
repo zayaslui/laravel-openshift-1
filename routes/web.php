553ca95ee8febbbb7872be5eadfcc23cebfdc6e8
Muhammad Zamroni <halo@matriphe.com>
2018-04-01 07:14:08 +0700

    Add readiness probe route

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Readiness Probe
+|--------------------------------------------------------------------------
+|
+| A readiness probe determines if a container is ready to service requests.
+| If the readiness probe fails a container, the endpoints controller ensures
+| the container has its IP address removed from the endpoints of all services.
+| A readiness probe can be used to signal to the endpoints controller that
+| even though a container is running, it should not receive any traffic
+| from a proxy. Set a readiness check by configuring the
+| template.spec.containers.readinessprobe stanza of a pod configuration.
+|
+*/
+Route::get('health.php', function () {
+    try {
+        \DB::connection()->getPdo();
+        return 'OK';
+    } catch (\Exception $e) {
+        abort(500, "Connection failed: " . $e->getMessage());
+    }
+});
