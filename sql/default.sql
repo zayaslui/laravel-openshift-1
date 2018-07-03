0c623369986e4c6ea4ad98b1277f9b6c9e1716c5
Luis Zayas <zayaslui@gmail.com>
2018-07-03 08:43:49 -0400

    update backup

---
+++ sql/default.sql
@@ -0,0 +1,33 @@
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(
+1,NULL,NULL,'descripcion ',80,'Ruta Santa Rosa - Capitán Bado','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Proyecto de Rehabilitaci&oacute;n y Pavimentaci&oacute;n del Tramo Santa Rosa del Aguaray &ndash; Capit&aacute;n Bado.&nb','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Proyecto de Rehabilitaci&oacute;n y Pavimentaci&oacute;n del Tramo Santa Rosa del Aguaray &ndash;');
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(2,NULL,NULL,'descripcion ',81,'Ruta Luque - San Bernardino','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.<br />
+Pavimentaci&oacute;n Asf&aacute;ltica del Tramo Luque &ndash; San Bernardino
+','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.<br />
+Pavimentaci&oacute;n Asf&aacute;ltica del Tramo Luque &ndash; San Bernardino');
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(3,NULL,NULL,'descripcion ',82,'Avda. Acceso Este Laguna Grande','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Accesos Viales a la Ciudad de Asunci&oacute;n.&nbsp;<br />
+&ndash;Paquete 1: Acceso Este a la Ciudad de Asunci&oacute;n<','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Accesos Viales a la Ciudad de Asunci&oacute;n.&nbsp;<br />
+&ndash;Paquete 1: Acceso Este a la Ciudad de Asunci&oacute;n<');
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(4,NULL,NULL,'descripcion ',84,'Franja Costera de Encarnación','<p>ENTIDAD BINACIONAL YACYRET&Aacute;.&nbsp;<br />
+Tratamiento &nbsp;Costero de la Ciudad de Encarnaci&oacute;n - Departamento de Itap&uacute;a.&nbsp;<br />
+&ndash;Paquete A.</p>
+
+<p>Movi','<p>ENTIDAD BINACIONAL YACYRET&Aacute;.&nbsp;<br />
+Tratamiento &nbsp;Costero de la Ciudad de Encarnaci&oacute;n - Departamento de Itap&uacute;a.&nbsp;');
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(5,NULL,NULL,'descripcion ',86,'Aeropuerto Internacional Guaraní','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Construcci&oacute;n del Aeropuerto Internacional Guaran&iacute;.&nbsp;<br />
+Contrato de Obras Civiles y Arquitect&oacut','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Construcci&oacute;n del Aeropuerto Internacional Guaran&iacute;.&nbsp;');
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(6,NULL,NULL,'descripcion ',88,'Aeropuerto de Encarnación','<p>ENTIDAD BINACIONAL YACYRET&Aacute;.&nbsp;<br />
+Construcci&oacute;n del Aeropuerto de Encarnaci&oacute;n - Dpto. de Itap&uacute;a.&nbsp;<br />
+-Pista de 2.000 mts de Longitud y 45 mts de','<p>ENTIDAD BINACIONAL YACYRET&Aacute;.&nbsp;<br />
+Construcci&oacute;n del Aeropuerto de Encarnaci&oacute;n - Dpto. de Itap&uacute;a.&nbsp;');
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(7,NULL,NULL,'descripcion ',89,'Avda. Madame Lynch','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Rehabilitaci&oacute;n de la Circunvalaci&oacute;n de Asunci&oacute;n, Tramo 4 Mojones &ndash; Defensores del Chaco &ndash','<p>MINISTERIO DE OBRAS P&Uacute;BLICAS Y COMUNICACIONES.&nbsp;<br />
+Rehabilitaci&oacute;n de la Circunvalaci&oacute;n de Asunci&oacute;n, Tramo 4 Mojones &ndash; Defensores del Chaco &ndash';
+insert into default.obras(id,created_at,updated_at,descripcion,imagen,titulo_obra,contenido_obra,introduccion)values(8,NULL,NULL,'descripcion ',90,Trayectoria,'<p>Para ver nuestra trayectoria, haga clic aqu&iacute;:&nbsp;<a href="https://goo.gl/l1e22D">https://goo.gl/l1e22D</a></p>
+','<p>Para ver nuestra trayectoria, haga clic aqu&iacute;:&nbsp;<a href="https://goo.gl/l1e22D">https://goo.gl/l1e22D</a></p>
+');
