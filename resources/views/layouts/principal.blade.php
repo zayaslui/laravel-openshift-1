254feb9f0ed559c9e47199b5d53f3719ed8d5455
Luis Zayas <zayaslui@gmail.com>
2018-07-10 23:22:12 -0400

    fix menu mobile

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -21,9 +21,9 @@
 <link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css"  media="all" />	
 <link href="{{asset('css/swipebox.css')}}" rel="stylesheet" type="text/css"  media="all" />	
 <!-- BEGIN JIVOSITE CODE {literal} -->
-{{-- <script type='text/javascript'>
+<script type='text/javascript'>
 (function(){ var widget_id = 'BC1lbLnA4G';var d=document;var w=window;function l(){
-var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script> --}}
+var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();</script>
 <!-- {/literal} END JIVOSITE CODE -->
 
  </head>
