2a30a5029a74a4e622dbebeb54d4652ca4a55441
Luis Zayas <zayaslui@gmail.com>
2018-07-15 17:31:31 -0400

    update compromiso

--- resources/views/compromiso.blade.php
+++ resources/views/compromiso.blade.php
@@ -17,23 +17,26 @@
 							El Grupo Empresarial EDB Construcciones – Constructora Asunción S.A. adopta como política de calidad la misión institucional de:
 							Respetar los compromisos contractuales asumidos, Cumplir con los plazos convenidos, Optimizar los costos sin descuidar la Calidad y Mejorar Continuamente el Sistema de Gestión, todo ello con el fin de lograr la satisfacción plena de los clientes y mantener el nombre del Grupo en el sitial en que se ha ubicado luego de mas de 40 años de trabajo.<br>
 						</p>
-						<p>
-							1- Mantener la certificación del sistema de gestión de calidad bajo las normas ISO 9001.
+						<p class="pcompromiso">
+						<span></span>
+						Mantener la certificación del sistema de gestión de calidad bajo las normas ISO 9001.
 						</p>
-						<p>
-							2- Capacitar a todos los funcionarios, para que desarrollen habilidades y conocimientos que le permitan mejorar su calidad de vida, sintiéndose a la vez participes y comprometidos con el Grupo.
+						<p class="pcompromiso">
+						<span></span>							
+						Capacitar a todos los funcionarios, para que desarrollen habilidades y conocimientos que le permitan mejorar su calidad de vida, sintiéndose a la vez participes y comprometidos con el Grupo.
 						</p>
-						<p>
-							3- Continuar con el compromiso de dar prioridad a los trabajadores, frente a cualquier otra obligación asumida. 
+						<p class="pcompromiso">
+						Continuar con el compromiso de dar prioridad a los trabajadores, frente a cualquier otra obligación asumida. 
 						</p>
-						<p>
-							4- Actualizar y perfeccionar permanentemente a las diferentes áreas del Grupo, en los avances y cambios tecnológicos, para obtener eficiencia en el uso de recursos.
+						<p class="pcompromiso">
+						<span></span>
+						Actualizar y perfeccionar permanentemente a las diferentes áreas del Grupo, en los avances y cambios tecnológicos, para obtener eficiencia en el uso de recursos.
 						</p>
-						<p>
-							5- Mantener condiciones de trabajo seguras, observando el cumplimiento de las leyes y normas de seguridad personal, higiene en el trabajo y señalización en sitio de obras. 
+						<p class="pcompromiso">
+						<span></span>Mantener condiciones de trabajo seguras, observando el cumplimiento de las leyes y normas de seguridad personal, higiene en el trabajo y señalización en sitio de obras. 
 						</p>
-						<p>
-							6- Cumplir las medidas y exigencias que determinan las leyes en lo que respecta a la preservación del Medio Ambiente.
+						<p class="pcompromiso">
+							<span></span>Cumplir las medidas y exigencias que determinan las leyes en lo que respecta a la preservación del Medio Ambiente.
 						</p>						
 					</div>
 				</div>
@@ -46,54 +49,71 @@
 					  <div class="services-header">
 						<h3>Misión - Visión</h3>
 					 </div>
-					   <div class="section group group">
-							<div class="listview_1_of_2 images_1_of_2">
-<!--  								<div class="listimg listimg_2_of_1">
-									  <img src="images/one.png">
-								</div> -->
-							    <div class="text list_2_of_1">
- 									<h3><span>Misión</span></h3>
-			                      <div class="blog-card spring-fever img1">
+					   <div class="section group">
+							{{-- primero --}}
+							<div class="listview_1_of_2_compromiso images_1_of_2">
+							    <div class="text list_2_of_1_compromiso">
+ 									<h3><span class="tab">Misión</span></h3>
+			                      <div class="blog-card-compromiso spring-fever img1">
 			                          <div class="title-content">
 			                            <!-- <h3 class="h3_">HORMIGON ELABORADO</h3> -->
 			                            <hr />
 			                            <div class="intro">EDBconstrucciones - Misión</div>
 			                          </div><!-- /.title-content -->
-			                          <div class="card-info">
+			                          <div class="card-info-compromiso">
 											Somos una empresa de ingeniería con gran experiencia y trayectoria en el ámbito de la construcción vial, construcciones aeroportuarias e ingeniería sanitaria, que brindamos construcciones de calidad, basados en el conocimiento adquirido a través de los años, perfeccionando y actualizando nuestros métodos, aplicando además procesos de control de calidad para asegurar la satisfacción y conformidad de nuestros clientes.
 			                          </div><!-- /.card-info -->
 			                          <!-- overlays -->
-			                          <div class="gradient-overlay"></div>
-			                          <div class="color-overlay"></div>
+			                          <div class="gradient-overlay-compromiso"></div>
+			                          <div class="color-overlay-compromiso"></div>
 			                          <a href="services/hormigon" class="ver">Ver mas</a>
 			                    </div><!-- /.blog-card -->
 
 							   </div>
 						   </div>
-
-							<div class="listview_1_of_2 images_1_of_2">
-<!--  								<div class="listimg listimg_2_of_1">
-									  <img src="images/dos.png">
-								</div> -->
-								<div class="text list_2_of_1">
-									  <h3><span>Visión </span></h3>
-			                      <div class="blog-card spring-fever img2">
+							
+							{{-- segundo --}}
+							<div class="listview_1_of_2_compromiso images_1_of_2">
+								<div class="text list_2_of_1_compromiso">
+ 									<h3><span class="tab">Visión</span></h3>
+			                      <div class="blog-card-compromiso spring-fever img2">
 			                          <div class="title-content">
-			                            <!-- <h3 class="h3_">HORMIGON ELABORADO</h3> -->
 			                            <hr />
 			                            <div class="intro">EDBconstrucciones - Visión</div>
 			                          </div><!-- /.title-content -->
-			                          <div class="card-info">
+			                          <div class="card-info-compromiso">
 												Ser el principal referente de las empresas paraguayas especializadas en la ingeniería vial y construcciones civiles apostar a la formación de jóvenes profesionales paraguayos.
 			                          </div><!-- /.card-info -->
 			                          <!-- overlays -->
-			                          <div class="gradient-overlay"></div>
-			                          <div class="color-overlay"></div>
+			                          <div class="gradient-overlay-compromiso"></div>
+			                          <div class="color-overlay-compromiso"></div>
 			                          <a href="services/mineria" class="ver">Ver mas</a>
 			                    </div><!-- /.blog-card -->
 
 								</div>
 							</div>
+
+							{{-- tercero --}}
+							<div class="listview_1_of_2_compromiso images_1_of_2">
+								<div class="text list_2_of_1_compromiso">
+ 									<h3><span class="tab">Objetivos</span></h3>
+			                      <div class="blog-card-compromiso spring-fever img2">
+			                          <div class="title-content">
+			                            <hr />
+			                            <div class="intro">EDBconstrucciones - Objetivos</div>
+			                          </div><!-- /.title-content -->
+			                          <div class="card-info-compromiso">
+												Brindar la satisfacción y conformidad a nuestros clientes. Seguir construyendo obras de calidad, sólidas y durables, aplicando sistemas de estándar internacional en el control y gestión de calidad. Establecer vínculos armónicos con el medio ambiente. Seguir colaborando en la formación de jóvenes técnicos y profesionales mediante el programa de pasantías estudiantiles habilitado para estudiantes universitarios y de educación media.
+			                          </div><!-- /.card-info -->
+			                          <!-- overlays -->
+			                          <div class="gradient-overlay-compromiso"></div>
+			                          <div class="color-overlay-compromiso"></div>
+			                          <a href="services/mineria" class="ver">Ver mas</a>
+			                    </div><!-- /.blog-card -->
+
+								</div>
+							</div>
+
 
 						</div>
 
