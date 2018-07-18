9112cc3539910b0195de04fdd6f3a7598f4a6e16
Luis Zayas <zayaslui@gmail.com>
2018-07-18 10:29:31 -0400

    update layers

--- sql/layersliders.sql
+++ sql/layersliders.sql
@@ -3,39 +3,43 @@ drop table sliders;
 drop table layersliders;
 
 create table layersliders(
-layerslider int auto_increment,
-tabla varchar(30), -- obras -servicios - otras cosas
-apertura varchar(255),
-cierre varchar(255),
-primary key(layerslider)
+	layerslider int auto_increment,
+	tabla varchar(30), -- obras -servicios - otras cosas
+	clase varchar(255),
+	style varchar(255),
+	primary key(layerslider)
 );
 
 create table sliders(
-slider int auto_increment,
-layerslider int,
-apertura varchar(255),
-cierre varchar(255),
-primary key(slider),
-foreign key(layerslider) references layersliders(layerslider) on update cascade on delete restrict
+	slider int auto_increment,
+	layerslider int,
+	clase varchar(255),
+	data_ls varchar(255),
+	primary key(slider),
+	foreign key(layerslider) references layersliders(layerslider) on update cascade on delete restrict
 );
 
 create table layers(
-layer int auto_increment,
-slider int,
-apertura varchar(255),
-cierre varchar(255),
-primary key(layer),
-foreign key(slider) references sliders(slider) on update cascade on delete restrict
+	layer int auto_increment,
+	slider int,
+	tipo varchar(255),
+	clase varchar(255),
+    data_ls varchar(255),
+	style varchar(255),
+	src varchar(255),
+    otros varchar(255),
+	descripcion varchar(255),
+	primary key(layer),
+	foreign key(slider) references sliders(slider) on update cascade on delete restrict
 );
 
 /*layersliders*/
-insert into layersliders values(1,'obras','<div id="layerslider" class="layerslider centrar" style="width: 1280px; height: 500px;">{}</div>','</div>');
+insert into layersliders values(1,'obras','layerslider centrar','width: 1280px; height: 500px;');
 
-insert into sliders values(1,1,'<div class="ls-slide" data-ls="slidedelay: 7000; transition2d: 75,79;">','</div>');
+insert into sliders values(1,1,'ls-slide','slidedelay: 7000; transition2d: 75,79;');
 
-insert into layers values(1,1,'<img src="/images/obras/81_1.jpg" class="ls-bg" alt="Slide background"/>','');
-insert into layers values(2,1,'<p class="ls-l" style="top:70px;left:10px;font-weight: 300;height:40px;padding-right:10px;padding-left:10px;font-size:30px;line-height:37px;color:#ffffff;background:#3D82DA;border-radius:4px;white-space: nowrap;" data-ls="durationin:1500;delayin:3300;rotatein:20;rotatexin:30;scalexin:1.5;scaleyin:1.5;transformoriginin:left 50% 0;durationout:750;rotateout:20;rotatexout:-30;scalexout:0;scaleyout:0;transformoriginout:left 50% 0;">Aeropuerto Internacional Guaraní','</p>');
-insert into layers values(3,1,'<p class="ls-l" style="top:130px;left:10px;font-weight: 300;height:40px;padding-right:10px;padding-left:10px;font-size:30px;line-height:37px;color:#ffffff;background:#3D82DA;border-radius:4px;white-space: nowrap;" data-ls="durationin:1500;delayin:4300;rotatein:20;rotatexin:30;scalexin:1.5;scaleyin:1.5;transformoriginin:left 50% 0;durationout:750;rotateout:20;rotatexout:-30;scalexout:0;scaleyout:0;transformoriginout:left 50% 0;">EDB Construcciones','</p>');
-
+insert into layers values(1,1,'img','ls-bg',null,null,'/images/obras/81_1.jpg','Slide background',null);
+insert into layers values(2,1,'p','ls-l','durationin:1500;delayin:3300;rotatein:20;rotatexin:30;scalexin:1.5;scaleyin:1.5;transformoriginin:left 50% 0;durationout:750;rotateout:20;rotatexout:-30;scalexout:0;scaleyout:0;transformoriginout:left 50% 0;','top:70px;left:10px;font-weight:300;height:40px;padding-right:10px;padding-left:10px;font-size:30px;line-height:37px;color:#ffffff;background:#3D82DA;border-radius:4px;white-space: nowrap;',null,null,'Aeropuerto Internacional Guaraní');
+insert into layers values(3,1,'p','ls-l','durationin:1500;delayin:4300;rotatein:20;rotatexin:30;scalexin:1.5;scaleyin:1.5;transformoriginin:left 50% 0;durationout:750;rotateout:20;rotatexout:-30;scalexout:0;scaleyout:0;transformoriginout:left 50% 0;','top:130px;left:10px;font-weight: 300;height:40px;padding-right:10px;padding-left:10px;font-size:30px;line-height:37px;color:#ffffff;background:#3D82DA;border-radius:4px;white-space: nowrap;',null,null,'EDB Construcciones');
 
 select *from layers;
