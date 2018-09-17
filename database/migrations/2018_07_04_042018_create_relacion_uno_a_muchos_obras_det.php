df5c4a1e89a95b690795d18d79d8ce07bedb8f21
Luis Zayas <zayaslui@gmail.com>
2018-09-17 16:36:24 -0400

    update migration

--- database/migrations/2018_07_04_042018_create_relacion_uno_a_muchos_obras_det.php
+++ database/migrations/2018_07_04_042018_create_relacion_uno_a_muchos_obras_det.php
@@ -13,14 +13,12 @@ class CreateRelacionUnoAMuchosObrasDet extends Migration
      */
     public function up()
     {
-        Schema::create('obras_det', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::table('obras_det', function (Blueprint $table) {
             $table->integer('obra_id')->unsigned();
             $table->foreign('obra_id')->references('id')->on('obras');
             $table->string('titulo_obra')->nullable();
             $table->string('contenido_obra')->nullable();
             $table->string('imagen')->nullable();
-            $table->timestamps();
         });
     }
 
